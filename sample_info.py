280a0da8abb12fd5978d042c9efef0293fc2b7c7
UnknownBotz <145199654+UnknownBotz@users.noreply.github.com>
2023-09-21 09:11:08 +0530

    Add files via upload

--- sample_info.py
+++ sample_info.py
@@ -2,8 +2,8 @@
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
 API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_HASH = ''
+BOT_TOKEN = ''
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,8 +11,8 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = []
+CHANNELS = []
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
