4b1c45a18428b49d921db1e3fc6e0a477b87771f
sachinkpt <145140896+sachinkpt@users.noreply.github.com>
2023-09-24 11:32:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rohaniscoder/Simple.git /NataliyaH
+  git clone https://github.com/sachinkpt/unknownSimpleFilter.git /NataliyaH
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /NataliyaH
